017dd892c71765750a3ffc233265fd6e16f98dec
Bilaal Latif <bilaallatif@outlook.com>
2023-08-15 20:17:08 +0100

    Added loop

--- rpc.js
+++ rpc.js
@@ -14,9 +14,26 @@ let desicion = (round) => {
     }
 }
 
-let playerSelection = prompt('Choose your weapon: Rock, Paper or Scissors?').toLowerCase();
-let computerSelection = getComputerChoice();
-console.log('Player: ' + playerSelection);
-console.log('Computer: ' + weapon[computerSelection]);
-console.log(desicion(round(computerSelection, weapon.indexOf(playerSelection))));
+let win_loss_counter = 0;
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt('Choose your weapon: Rock, Paper or Scissors?').toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log('Player: ' + playerSelection);
+    console.log('Computer: ' + weapon[computerSelection]);
+    let win_loss = round(computerSelection, weapon.indexOf(playerSelection));
+    console.log(desicion(win_loss));
+
+    win_loss_counter += win_loss;
+    console.log('Current score: ' + win_loss_counter);
+}
+
+if (win_loss_counter > 0) {
+    console.log('You win the game!');
+}
+else if (win_loss_counter < 0) {
+    console.log('You lose the game!');
+}
+else {
+    console.log('Draw!');
+}
 
